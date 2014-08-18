d8242bd60d49007968a8fd8615df123d9b708ca2
Thomas Khyn <thomas@ksytek.com>
2014-08-18 18:18:08 +1200

    Better way to retrieve objects (sort by content type to optimize db access)

--- gm2m/query.py
+++ gm2m/query.py
@@ -1,18 +1,29 @@
-from django.db.models.query import QuerySet
+from collections import defaultdict
 
-from .models import FK_ATTNAME
+from django.db.models.query import QuerySet
+from django.contrib.contenttypes.models import ContentType
+
+from .models import CT_ATTNAME, PK_ATTNAME
 
 
 class GM2MQuerySet(QuerySet):
     """
-    A QuerySet with a fetch_generic_relations() method to bulk fetch
-    all generic related items.  Similar to select_related(), but for
-    generic foreign keys. This wraps QuerySet.prefetch_related.
+    A QuerySet for GM2M models which yields actual objects instead of GM2M
+    objects when iterated over
     """
 
     def iterator(self):
         """
-        Override to return the actual object, not the GM2MObject
+        Override to return the actual objects, not the GM2MObject
+        Fetch the actual objects by content types to optimize database access
         """
-        for i in super(GM2MQuerySet, self).iterator():
-            yield getattr(i, FK_ATTNAME)
+        ct_pks = defaultdict(lambda: [])
+        for ct, pk \
+        in super(GM2MQuerySet, self).values_list(CT_ATTNAME, PK_ATTNAME):
+            ct_pks[ct].append(pk)
+
+        for ct, pks in ct_pks.iteritems():
+            for __, obj in ContentType.objects.get_for_id(ct).model_class() \
+                                              ._default_manager.in_bulk(pks) \
+                                              .iteritems():
+                yield obj
