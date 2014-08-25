917898a4eca2acc4eeea1546ef452a65c4d0d3e7
Thomas Khyn <thomas@ksytek.com>
2014-08-25 22:15:17 +1200

    Model classes are now completely reloaded on test case setup

--- tests/base.py
+++ tests/base.py
@@ -1,9 +1,18 @@
+import sys
+import inspect
+
 from django import test
 from django.conf import settings
-from django.db.models import loading
+from django.db import models
 from django.utils.datastructures import SortedDict
 from django.core.management import call_command
 from django.utils import six
+from django.db.models.fields import related
+
+from gm2m.descriptors import GM2MRelatedDescriptor, \
+                             ReverseGM2MRelatedDescriptor
+
+from . import models as test_models
 
 # no nose tests here !
 __test__ = False
@@ -36,16 +45,29 @@ class TestSettingsManager(object):
                 apps.set_installed_apps(kwargs['INSTALLED_APPS'])
             except ImportError:
                 pass
+
+            # cleanup app models cache
+            changed_apps = set(kwargs['INSTALLED_APPS']).difference(
+                               self._original_settings['INSTALLED_APPS'])
+            for app in changed_apps:
+                app = app.split('.')[-1]
+                if app in models.loading.cache.app_models:
+                    del(models.loading.cache.app_models[app])
+
             self.syncdb()
 
+            for app in changed_apps:
+                models.loading.cache.register_models(app)
+
     def syncdb(self):
-        loading.cache.loaded = False
-        loading.cache.app_store = SortedDict()
-        loading.cache.handled = set()
-        loading.cache.postponed = []
-        loading.cache.nesting_level = 0
-        loading.cache._get_models_cache = {}
-        loading.cache.available_apps = None
+        cache = models.loading.cache
+        cache.loaded = False
+        cache.app_store = SortedDict()
+        cache.handled = set()
+        cache.postponed = []
+        cache.nesting_level = 0
+        cache._get_models_cache = {}
+        cache.available_apps = None
 
         call_command('syncdb', verbosity=0)
 
@@ -77,12 +99,30 @@ class TestCase(test.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        global test_models
         module = cls.__module__
-        app = module[:module.rfind('.')]
+        app_module = module[:module.rfind('.')]
         cls.settings_manager = TestSettingsManager()
+        test_models = reload(test_models)
+        if app_module + '.models' in sys.modules:
+            # if that's the 2nd class of the module, the models module should
+            # be reloaded during syncdb
+            del(sys.modules[app_module + '.models'])
+
         cls.settings_manager.set(
-            INSTALLED_APPS=settings.INSTALLED_APPS + (app,))
+            INSTALLED_APPS=settings.INSTALLED_APPS + (app_module,))
+
+        # reload the model classes in the test module, if any, so that the
+        # instances in the test are created using the updated classes
+        for m in dir(sys.modules[module]):
+            modcls = getattr(sys.modules[module], m)
+            if inspect.isclass(modcls) and issubclass(modcls, models.Model) \
+            and not modcls._meta.abstract:
+                setattr(sys.modules[module], m,
+                        getattr(sys.modules[modcls.__module__],
+                                modcls.__name__))
 
     @classmethod
     def tearDownClass(cls):
         cls.settings_manager.revert()
+        related.pending_lookups = {}
