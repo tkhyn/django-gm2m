f7bbd8a8ec597d34fd9e8b88e8f4e8699f4d753b
Thomas Khyn <thomas@ksytek.com>
2014-10-03 22:37:55 +1300

    get_content_type helper function can now take model classes as argument

--- gm2m/helpers.py
+++ gm2m/helpers.py
@@ -2,4 +2,11 @@ from django.contrib.contenttypes.models import ContentType
 
 
 def get_content_type(obj):
-    return ContentType.objects.db_manager(obj._state.db).get_for_model(obj)
+    ct_mngr = ContentType.objects
+    try:
+        # obj is a model instance, retrieve database
+        qs = ct_mngr.db_manager(obj._state.db)
+    except AttributeError:
+        # obj is a model class
+        qs = ct_mngr
+    return qs.get_for_model(obj)
