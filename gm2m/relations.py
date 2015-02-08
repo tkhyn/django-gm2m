c88813306fdba43df60e57ff63cfbc8f64b564aa
Thomas Khyn <thomas@ksytek.com>
2015-02-08 20:35:18 +1300

    Adds blank, editable and serialize init kwargs to GM2MRelation
    (fixes ModelForm issues)

--- gm2m/relations.py
+++ gm2m/relations.py
@@ -42,6 +42,11 @@ class GM2MRelation(ForeignObject):
     def __init__(self, to, field, rel, **kwargs):
         self.field = field
         kwargs['rel'] = rel
+
+        kwargs['blank'] = True
+        kwargs['editable'] = False
+        kwargs['serialize'] = False
+
         super(GM2MRelation, self).__init__(to, from_fields=[field.name],
                                            to_fields=[], **kwargs)
 
