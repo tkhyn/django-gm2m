575bbd82e9960e91b2e75ba8218e64c63edf7fcc
Thomas Khyn <thomas@ksytek.com>
2015-12-12 02:10:24 +1300

    Adds a ROOT_URLCONF to please Django 1.9

--- tests/settings.py
+++ tests/settings.py
@@ -17,3 +17,5 @@ INSTALLED_APPS = (
 MIDDLEWARE_CLASSES = ()  # so that Django 1.7 does not complain
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+ROOT_URLCONF = 'tests.urls'
---
+++ tests/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url, patterns
+
+
+urlpatterns = patterns('',
+    url('^$', lambda: None)
+)
