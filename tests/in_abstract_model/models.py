7ac3e5be77e499d62415524eeff520be729f6de8
Thomas Khyn <thomas@ksytek.com>
2016-02-24 15:29:57 +1300

    [re #22] Adds tests for abstract models

---
+++ tests/in_abstract_model/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+import gm2m
+
+
+class LinksBase(models.Model):
+
+    class Meta:
+        abstract = True
+        app_label = 'in_abstract_model'
+
+    related_objects = gm2m.GM2MField()
+
+
+class BasicLinks(LinksBase):
+    pass
+
+
+class EnhancedLinks(LinksBase):
+    pass
